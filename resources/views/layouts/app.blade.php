677dc610288666cde7df07d51c4c23cab3560fb7
iwan <iwanteguh1998@gmail.com>
2023-01-26 09:33:02 +0700

    add new logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>Telkom Participant</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('dist/img/TELKOM/favicon.ico') }}" type="image/x-icon">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -94,9 +95,9 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="" class="brand-link">
-                <img src="{{ asset('') }}dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">MyApp</span>
+                <img src="{{ asset('dist/img/TELKOM/hand.png') }}" alt="AdminLTE Logo"
+                    class="brand-image" style="opacity: .8">
+                <span class="brand-text font-weight-light">My IndiHome POS</span>
             </a>
 
             <!-- Sidebar -->
@@ -104,7 +105,7 @@
                 <!-- Sidebar user (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ asset('') }}dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                        <img src="{{ asset('dist/img/avatar5.png') }}" class="img-circle elevation-2"
                             alt="User Image">
                     </div>
                     <div class="info">
@@ -291,7 +292,7 @@
             <div class="float-right d-none d-sm-block">
                 <b>Version</b> 3.2.0
             </div>
-            <strong>Copyright &copy; {{ date('Y') }}-{{ date('Y') + 1 }} <a href="">Telkom</a>.</strong> All rights
+            <strong>Copyright &copy; {{ date('Y') }} <a href="">Telkom</a>.</strong> All rights
             reserved.
         </footer>
 
