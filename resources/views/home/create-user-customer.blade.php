2c21f29c61329545aaf48ce96e74a44af6515b7c
Yusron <yuzyus268@gmail.com>
2023-02-27 14:30:40 +0700

    Update add jquery

--- resources/views/home/create-user-customer.blade.php
+++ resources/views/home/create-user-customer.blade.php
@@ -118,6 +118,7 @@
     </div>
 </body>
 @include('sweetalert::alert')
+<script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
     $('.select2').select2();
